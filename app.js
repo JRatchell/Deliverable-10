3688d7051153adf41a65e3fb820146bf7da5524d
JRatchell <jrboston1@gmail.com>
2022-12-05 16:00:23 -0500

    Moved functions to app.js

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-scriptsource=
+function bold(){document.getElementById('textarea1').style.fontWeight='bold';}
+function italic(){document.getElementById('textarea1').style.fontStyle='italic';}
+function la(){document.getElementById('textarea1').style.textAlign='left';}
+function ra(){document.getElementById('textarea1').style.textAlign='right';}
+function center(){document.getElementById('textarea1').style.textAlign='center';}
+function uppercase(){document.getElementById('textarea1').style.textTransform='uppercase';}
+function lowercase(){document.getElementById('textarea1').style.textTransform='lowercase';}
+function capitalize(){document.getElementById('textarea1').style.textTransform='capitalize';}
+function clear(){document.getElementById('textarea1').value= '';}
